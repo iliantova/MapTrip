51a985b97f996fd1bc79ebb96dc72ced4e628f91
iliantova <iliyana.antova@gmail.com>
2016-02-18 14:20:15 +0200

    register button image

--- MapTrip/app/view/register/register.js
+++ MapTrip/app/view/register/register.js
@@ -1,5 +1,8 @@
 var frameModule = require("ui/frame");
 var pagesModule = require("ui/page");
+var view = require("ui/core/view");
+var buttonModule = require("ui/button");
+var button = new buttonModule.Button();
 
 var pageModules = (function() {
 
@@ -8,9 +11,16 @@ var pageModules = {
         
         pageLoaded: function(args) {
             var page = args.object;
+             var usernameTextField = view.getViewById(page, "username");
+    var passwordTextField = view.getViewById(page, "password");
+    var confirmTextField = view.getViewById(page, "confirmpassword");
+    var signUpButton = view.getViewById(page, "signUpButton");
+    signUpButton.backgroundImage = "~/app/view/register/gradient_button.png";
            topmost = frameModule.topmost();
-        },
-        
+
+          // if(passwordTextField.text === confirmTextField.text || usernameTextField.text === "" || passwordTextField.text === "")
+       },
+            
         }
          return pageModules;
 })();
